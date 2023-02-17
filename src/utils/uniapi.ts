2094a54c12d37d3df61f3f2f6e08e9e91dddeed1
hatiwowo <hatiwowo@163.com>
2023-02-17 14:21:08 +0800

    完成showToast逻辑

--- src/utils/uniapi.ts
+++ src/utils/uniapi.ts
@@ -1,8 +1,30 @@
-type ToastIcon = "none" | "success" | "loading" | "error";
-export function toast(title: string, icon: ToastIcon = "none") {
+type ToastType = "none" | "success" | "loading" | "error";
+type ToastOptions = { title: string; icon?: ToastType; duration?: number };
+type ToastFn = (params: ToastOptions | string) => void;
+type toastMethod = (title: string) => void;
+interface Toast extends ToastFn {
+    success: toastMethod;
+    error: toastMethod;
+    loading: toastMethod;
+}
+function MyToast(params: ToastOptions | string) {
+    const options: ToastOptions = { title: "", icon: "none", duration: 2000 };
+    if (typeof params === "string") {
+        options.title = params;
+    } else {
+        Object.assign(options, params);
+    }
     uni.showToast({
-        icon,
-        title,
-        duration: 2000
+        icon: options.icon,
+        title: options.title,
+        duration: options.duration
     });
 }
+export const Toast: ToastFn & Partial<Toast> = MyToast;
+const toastMethods = ["success", "error", "loading"] as const;
+toastMethods.forEach((method) => {
+    Toast[method] = (title) => {
+        const options: ToastOptions = { title, icon: method, duration: 2000 };
+        return Toast(options);
+    };
+});
