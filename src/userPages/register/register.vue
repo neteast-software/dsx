a1db4a1399c9eddf751913901ed7901f615eee76
hatiwowo <hatiwowo@163.com>
2023-02-21 16:28:31 +0800

    添加uni-ui

--- src/userPages/register/register.vue
+++ src/userPages/register/register.vue
@@ -4,7 +4,7 @@
             <image src="@/static/login/logo.png" alt=""></image>
             <view class="text">欢迎来到抖省心</view>
         </view>
-        <view class="form">
+        <!-- <view class="form">
             <view class="tel"> <input type="text" placeholder="请输入手机号" /> </view>
             <view class="verify">
                 <input type="text" placeholder="请输入验证码" />
@@ -12,14 +12,62 @@
             </view>
             <view> <input type="text" placeholder="设置登录密码(6-20位数字或字母)" /> </view>
             <view> <input type="text" placeholder="请输入邀请码" /> </view>
-        </view>
-        <view class="btn-register">注册</view>
+        </view> -->
+        <uni-forms ref="form" :modelValue="formData" :rules="formRules">
+            <uni-forms-item class="form-item" name="phoneNumber">
+                <input v-model="formData.phoneNumber" class="form-item__input" type="text" placeholder="请输入手机号" />
+                <!-- <uni-easyinput type="text" v-model="formData.phoneNumber" placeholder="请输入手机号"></uni-easyinput> -->
+            </uni-forms-item>
+        </uni-forms>
+        <button class="btn-register" @click="register">注册</button>
     </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import uniForms from "@dcloudio/uni-ui/lib/uni-forms/uni-forms.vue";
+import uniFormsItem from "@dcloudio/uni-ui/lib/uni-forms-item/uni-forms-item.vue";
+import uniEasyinput from "@dcloudio/uni-ui/lib/uni-easyinput/uni-easyinput.vue";
+import { reactive, ref } from "vue";
+const formData = reactive({
+    phoneNumber: "",
+    verifyCode: "",
+    password: "",
+    inviteCode: ""
+});
+const formRules = reactive({
+    phoneNumber: {
+        rules: [
+            {
+                required: true,
+                errorMessage: "请输入姓名"
+            },
+            {
+                minLength: 3,
+                maxLength: 5,
+                errorMessage: "姓名长度在 {minLength} 到 {maxLength} 个字符"
+            }
+        ]
+    }
+});
+const form = ref<any>(null);
+function register() {
+    console.log("表单元素", form.value);
+    form.value.validate();
+    console.log(formData);
+}
+</script>
 
 <style scoped lang="scss">
+.form-item {
+    width: 100%;
+    height: 98rpx;
+    border-radius: 999rpx;
+    background: #f5f5f5;
+}
+.form-item__input {
+    background-color: transparent;
+    height: 100%;
+}
 .wel {
     position: relative;
     margin-top: 70rpx;
