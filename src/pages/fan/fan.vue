afa8b7ed1ea104630e518ee0f616bb47d0c4a65a
chenchen <1875729224@qq.com>
2023-02-23 15:28:30 +0800

    完成粉丝切图

---
+++ src/pages/fan/fan.vue
@@ -0,0 +1,50 @@
+<template>
+    <view class="wrap grey">
+        <view class="account flex-center">
+            <image class="avatar" src="@/static/manage/avatar1.png" mode="aspectFill"></image>
+            <view class="message flex-rest-width">
+                <view class="message-item flex-center">
+                    <view class="name">王大锤</view>
+                    <view class="shopkeeper">店主</view>
+                </view>
+                <view class="tel">138 **** 1954</view>
+                <view class="stop">有效店铺数: 12</view>
+            </view>
+            <view class="copy-wrap flex-column">
+                <view class="copy">复制</view>
+                <view class="data">2023.02.17. 11:58:21</view>
+            </view>
+        </view>
+
+        <view class="account flex-center">
+            <image class="avatar" src="@/static/manage/avatar1.png" mode="aspectFill"></image>
+            <view class="message flex-rest-width">
+                <view class="message-item flex-center">
+                    <view class="name">王大锤</view>
+                    <view class="shopkeeper">店主</view>
+                </view>
+                <view class="tel">138 **** 1954</view>
+                <view class="stop">有效店铺数: 12</view>
+            </view>
+            <view class="copy-wrap flex-column">
+                <view class="copy">复制</view>
+                <view class="data">2023.02.17. 11:58:21</view>
+            </view>
+        </view>
+        <view class="invite flex-center">
+            <view class="recommender flex-center">
+                <image class="img-recommender" mode="widthFix" src="@/static/manage/recommender.png"></image>
+                <text>我的邀请人为：</text>
+            </view>
+            <view>
+                <text>王大锤一号 138 **** 2344</text>
+            </view>
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "./fan.scss";
+</style>
---
+++ src/pages/integral/integral.vue
@@ -0,0 +1,36 @@
+<template>
+    <view class="wrap grey">
+        <view class="account flex-box">
+            <view class="share">
+                <text class="text-share">分享获得积分</text>
+                <view class="data-surplus">
+                    <text class="data">时间: 2023-01-16 16:15:44 </text>
+                    <view class="surplus">
+                        <text>剩余积分：</text>
+                        <text class="text-surplus">998</text>
+                    </view>
+                </view>
+            </view>
+            <text class="text-integral">+ 998</text>
+        </view>
+        <view class="account flex-box">
+            <view class="share">
+                <text class="text-share">视频合成</text>
+                <view class="data-surplus">
+                    <text class="data">时间: 2023-01-16 16:15:44 </text>
+                    <view class="surplus">
+                        <text>剩余积分：</text>
+                        <text class="text-surplus">128</text>
+                    </view>
+                </view>
+            </view>
+            <text class="text-negative">- 32</text>
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "./integral.scss";
+</style>
