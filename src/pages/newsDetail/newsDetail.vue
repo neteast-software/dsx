be346341a2445b1dbc1bad2d7a984059da5bbfcc
chenchen <1875729224@qq.com>
2023-02-24 11:49:23 +0800

    消息详情完成

---
+++ src/pages/newsDetail/newsDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    <view class="details">
+        <view class="title">
+            <view class="h1">封号通知</view>
+            <text class="time">2023-01-28 10:32</text>
+        </view>
+        <view class="text-content">
+            <text style="white-space: pre-line"
+                >尊敬的用户 你好\n
+                这是一封封号邮件，由于您近期多次违规操作导致平台其他用户资金亏损。我们将对您进行为期三年的封号处理，请您规范用户行为，如有疑问请咨询客服\n
+                这是一封封号邮件，由于您近期多次违规操作导致平台其他用户资金亏损。我们将对您进行为期三年的封号处理，请您规范用户行为，如有疑问请咨询客服</text
+            >
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "./message.scss";
+</style>
---
+++ src/pages/search/search.vue
@@ -0,0 +1,20 @@
+<template>
+    <view class="wrap small">
+        <view class="header flex-center">
+            <view class="search-input flex-center">
+                <view class="line"></view>
+                <input
+                    type="text"
+                    placeholder="请搜索内容"
+                />
+            </view>
+            <view>取消</view>
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "./search.scss";
+</style>
